13f217a33c5ee41f4bb6753f22c2933bbfa0ae8b
firat <f1r4tc@gmail.com>
2020-08-11 02:36:16 +0300

    Add simple widget tests.

--- test/list_tile_switch_test.dart
+++ test/list_tile_switch_test.dart
@@ -1,1 +1,33 @@
-void main() {}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:list_tile_switch/list_tile_switch.dart';
+
+void main() {
+	testWidgets('Finds widgets on ListTileSwitch', (WidgetTester tester) async {
+		Widget testWidget = MaterialApp(
+			home: Material(
+			  child: Directionality(
+			  	textDirection: TextDirection.ltr,
+			  	child: ListTileSwitch(
+			  		value: true,
+			  		title: Text('Title'),
+			  		subtitle: Text('Subtitle'),
+			  		leading: Icon(Icons.message),
+			  		switchActiveColor: Colors.pink,
+			  		switchType: SwitchType.cupertino,
+			  		onChanged: (value) {
+			  		
+			  		},
+			  	),
+			  ),
+			),
+		);
+		await tester.pumpWidget(testWidget);
+		expect(find.text('Title'), findsOneWidget);
+		expect(find.text('Subtitle'), findsOneWidget);
+		expect(find.byIcon(Icons.message), findsOneWidget);
+		expect(find.byType(ListTile), findsOneWidget);
+		expect(find.byType(CupertinoSwitch), findsOneWidget);
+	});
+}
